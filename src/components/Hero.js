040c849c27ceeac64e8c17fc3a3da6e6ebba48f1
ThasaraSurage <sthasara26@gmail.com>
2025-03-04 22:04:57 +0530

    Updated code for Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,27 +4,23 @@ const HeroSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 2rem 1rem;
   position: relative;
   overflow: hidden;
-  min-height: 100vh;
-
-  @media (max-width: ${(props) => props.theme.breakpoints.md}) {
-    padding: 1rem;
-  }
+  height: calc(100vh - 80px); // Subtract navbar height
+  padding: 0;
 `
 
 const HeroContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   text-align: center;
   z-index: 2;
   width: 100%;
   max-width: 1200px;
-  position: relative;
-  margin-top: 2rem;
+  padding-top: 2rem;
+  height: 100%;
 `
 
 const HiTag = styled.div`
@@ -32,7 +28,7 @@ const HiTag = styled.div`
   padding: 0.5rem 1.5rem;
   border-radius: 25px;
   font-size: ${(props) => props.theme.fontSizes.fontsm};
-  margin-bottom: 1.5rem;
+  margin-bottom: 1rem;
   position: relative;
   border: 1px solid #eee;
   font-weight: 500;
@@ -50,16 +46,12 @@ const HiTag = styled.div`
 `
 
 const Title = styled.h1`
-  font-size: clamp(2.5rem, 5vw, 3.5rem);
+  font-size: clamp(2rem, 4vw, 3rem);
   font-weight: bold;
   margin: 0;
   line-height: 1.2;
-  margin-bottom: 2rem;
+  margin-bottom: 1rem;
   max-width: 800px;
-
-  @media (max-width: ${(props) => props.theme.breakpoints.md}) {
-    font-size: ${(props) => props.theme.fontSizes.fontxxl};
-  }
 `
 
 const ColoredText = styled.span`
@@ -69,36 +61,32 @@ const ColoredText = styled.span`
 const ImageContainer = styled.div`
   position: relative;
   width: 100%;
-  height: 500px;
+  flex-grow: 1;
   display: flex;
   justify-content: center;
   align-items: flex-end;
-  margin-top: 2rem;
-
-  @media (max-width: ${(props) => props.theme.breakpoints.md}) {
-    height: 400px;
-  }
+  margin-top: 1rem;
 `
 
 const SemiCircle = styled.div`
-  width: 600px;
-  height: 300px;
+  width: 500px;
+  height: 250px;
   background-color: #FFB74D;
-  border-radius: 300px 300px 0 0;
+  border-radius: 250px 250px 0 0;
   position: absolute;
   bottom: 0;
   z-index: 1;
 
   @media (max-width: ${(props) => props.theme.breakpoints.md}) {
-    width: 400px;
-    height: 200px;
-    border-radius: 200px 200px 0 0;
+    width: 300px;
+    height: 150px;
+    border-radius: 150px 150px 0 0;
   }
 `
 
 const ProfileImage = styled.div`
-  width: 400px;
-  height: 500px;
+  width: 300px;
+  height: 375px;
   background-image: url("/Thasara_Surage.png");
   background-size: cover;
   background-position: center top;
@@ -107,27 +95,27 @@ const ProfileImage = styled.div`
   margin-bottom: -5px;
 
   @media (max-width: ${(props) => props.theme.breakpoints.md}) {
-    width: 300px;
-    height: 375px;
+    width: 200px;
+    height: 250px;
   }
 `
 
 const Decoration = styled.div`
   position: absolute;
-  width: 50px;
-  height: 50px;
+  width: 40px;
+  height: 40px;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23FFB74D'%3E%3Cpath d='M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z'/%3E%3C/svg%3E");
   background-size: contain;
   opacity: 0.5;
   
   &.top-left {
-    top: 15%;
+    top: 10%;
     left: 15%;
     transform: rotate(-15deg);
   }
   
   &.bottom-right {
-    bottom: 25%;
+    bottom: 30%;
     right: 15%;
     transform: rotate(15deg);
   }
@@ -153,5 +141,5 @@ const Hero = () => {
   )
 }
 
-export default Hero;
+export default Hero
 
